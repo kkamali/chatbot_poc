44666985d44d6379b8c4c7fe31247df95250a605
kkamali <kathleenkamali@gmail.com>
2017-03-31 11:13:22 -0700

    chaged server.post

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-server.post('/api/messages', connector.listen());
+server.post('/API/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector);
 
